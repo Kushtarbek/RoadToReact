a6cc8d3bbb1951369f497a4a354fb978dfdc4c20
Kushtarbek <dr.kushtarbek@gmail.com>
2021-02-21 21:35:20 -0600

    updates

--- src/App.js
+++ src/App.js
@@ -1,56 +1,86 @@
-import logo from "./logo.svg";
-import "./App.css";
-import List from "./components/List";
+import React from "react";
 
-// let str = 'text' //string
-// let str1;        //undefined
-// let n = null;    //object
-// let bool = true;
-// let num = "123"
-// let bigint = "12312312"
-// let arr = [12,45,67,89];
+const App = () => {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+  const [searchTerm, setSearchTerm] = React.useState(
+    localStorage.getItem("search") || "React"
+  );
 
-const users = [
-  // { id: 1, firstName: "kushu", lastName: "the developer" },
-  { id: null, firstName: null, lastName: null },
-];
+  React.useEffect(() => {
+    localStorage.setItem("search", searchTerm);
+  }, [searchTerm]);
 
-function App() {
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const searchedStories = stories.filter((story) => {
+    const filteredSearch = story.title
+      .toLowerCase()
+      .includes(searchTerm.toLocaleLowerCase());
+    console.log(searchTerm, filteredSearch);
+    return filteredSearch;
+  });
+  /*filter(function(story) {return story.title.includes(searchTerm);})
+    The filter function takes a function as an argument, which accesses each item in the array and returns true or
+    false. If the function returns true, meaning the condition is met, the item stays in the newly created
+    array; if the function returns false, it’s removed*/
+
   return (
     <div>
-      <h1>My hacker stories</h1>
+      <h1>My Hacker Stories</h1>
 
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <Search search={searchTerm} onSearch={handleSearch} />
 
       <hr />
-      <List />
+
+      <List list={searchedStories} />
     </div>
   );
-}
+};
 
-// function List({ list }) {
-//   if (!list) {
-//     return null;
-//   }
+//----------Search Component
+//const { search, onSearch } = props;    //props object destructuring
+const Search = ({ search, onSearch }) => {
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" value={search} onChange={onSearch} />
+    </div>
+  );
+};
+//              UI -> Side-Effect -> State -> UI ...
 
-//   return (
-//     <div>
-//       {!list.length ? (
-//         <p>Sorry, the list is empty.</p>
-//       ) : (
-//         list.map((item) => <Item item={item} />)
-//       )}
-//     </div>
-//   );
-// }
+//----------List Component
+const List = ({ list }) =>
+  list.map((item) => <Item key={item.objectID} item={item} />);
 
-// function Item({ item }) {
-//   return (
-//     <li>
-//       {item.firstName} {item.lastName}
-//     </li>
-//   );
-// }
+const Item = ({ title, url, author, num_comments, points }) => (
+  <div>
+    <span>
+      <a href={url}> {title} </a>
+    </span>
+    <span> {author} </span>
+    <span> {num_comments} </span>
+    <span> {points} </span>
+  </div>
+);
 
 export default App;
---
+++ src/filter.js
@@ -0,0 +1,13 @@
+const words = [
+  "spray",
+  "limit",
+  "elielitete",
+  "exuberant",
+  "destruction",
+  "present",
+];
+
+const filteredArr = words.filter((word) => {
+  return word.length > 6;
+});
+console.log(filteredArr);
